a9571efc58d02efc6ce668d86d7dd491ad79b46b
ebooshi <luis_mi1999@ciencias.unam.mx>
2021-07-11 19:16:50 -0500

    Codigo corregido.

--- Practicas/Practica 2 Frama-C/sorts.c
+++ Practicas/Practica 2 Frama-C/sorts.c
@@ -1,17 +1,21 @@
- void swap(int *xp, int *yp) //esta funcion no sirve
+ #include <stdio.h>
+
+void swap(int *xp, int *yp)
 {
-	*xp = *yp;
-	*yp = *xp;
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
+ 
 
 void selectionSort(int arr[], int n)
 {
 	int i, j, min_idx;
 
-	for (i = 0; i < n-1; i++) //aqui es n
+	for (i = 0; i < n-1; i++)
 	{
 		min_idx = i;
-		for (j = i; j < n; j++){
+		for (j = i+1; j < n; j++){
 			if (arr[j] < arr[min_idx]){
 				min_idx = j;
 			}
@@ -24,17 +28,26 @@ void selectionSort(int arr[], int n)
 void insertionSort(int arr[], int n)
 {
     int i, key, j;
-    for (i = 1; i < n-1; i++) { //aqui es n
+    for (i = 1; i < n; i++) { //aqui es n
         key = arr[i];
-        j = i + 1; // aqui es menos
+        j = i - 1; // aqui es menos
 
         while (j >= 0 && arr[j] >= key) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
-        arr[j] = key;
+        arr[j+1] = key;
     }
 }
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
 
 int main()
 {
@@ -44,5 +57,7 @@ int main()
 	int arr2[] = {64, 25, 12, 22, 11};
 	int n2 = sizeof(arr2)/sizeof(arr[0]);
 	insertionSort(arr2, n2-10);
+    printArray(arr, n);
+    printArray(arr2, n2);
 	return 0;
 }
